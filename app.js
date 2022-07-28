ed77d9ccab64780d02a53c6d08c309ca4b09defe
Harshaja Agnihotri <hn.agnihotri29@gmail.com>
2022-07-28 22:49:28 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+
+const { query } = require("express");
+const express = require("express");
+const https = require("https");
+const { dirname } = require("path");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get("/", function(req, res) {
+   
+   res.sendFile(__dirname + "/index.html");
+
+});
+
+app.post("/", function(req, res) {
+    console.log(req.body.cityName); 
+
+    const query = req.body.cityName;
+   const apiKey = "afe2a9d5c7cf4f34a7695a3e2ff07974";
+   const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=metric";
+
+   https.get(url, function(response) {
+    console.log(response.statusCode);
+
+    response.on("data", function(data) {
+        const weatherData = JSON.parse(data);
+        const temp = weatherData.main.temp;
+        const weatherDesc = weatherData.weather[0].description;
+        const icon = weatherData.weather[0].icon
+        const imageUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+        res.write("<p>The Weather is currently " + weatherDesc + "</p>"); 
+        res.write("<h1>The Temperature in " + query +" is " + temp + "degree Celsius.</h1>");
+        res.write("<img src="+ imageUrl + ">");
+        res.send();
+    })
+   })
+})
+
+app.listen(3000, function() {
+    console.log("Server in running in port 3000");
+})
